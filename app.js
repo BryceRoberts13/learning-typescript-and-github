2fcbfab8aadbcfb5d1110594a26488fabe1ceb97
Bryce <llachlanroberts@gmail.com>
2025-01-31 21:52:29 -0800

    initial commit??

---
+++ app.js
@@ -0,0 +1,6 @@
+var message = 'Hello World!';
+var age = 20;
+var isAlive = true;
+console.log('type:', typeof message, 'value:', message);
+console.log('type:', typeof age, 'value:', age);
+console.log('type:', typeof isAlive, 'value:', isAlive);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let message: string = 'Hello World!';
+let age: number = 20;
+let isAlive: boolean = true;
+console.log('type:', typeof message, 'value:', message);
+console.log('type:', typeof age, 'value:', age);
+console.log('type:', typeof isAlive, 'value:', isAlive);
